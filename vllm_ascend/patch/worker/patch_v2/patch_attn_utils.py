6cce952204d836971631682c36f76db48be02597
Liam <ml646@duke.edu>
2026-08-05 11:04:13 +0800

    Revert "fix: patch MRV2 runner KV spec method"
    This reverts commit 1d65c0d3ffb6c80a2edca002fe2d9c056ae195f3.

--- vllm_ascend/patch/worker/patch_v2/patch_attn_utils.py
+++ vllm_ascend/patch/worker/patch_v2/patch_attn_utils.py
@@ -1,5 +1,4 @@
 import vllm
-import vllm.v1.worker.gpu_model_runner
 
 from vllm_ascend.worker.v2.attn_utils import (
     _allocate_kv_cache,
@@ -7,17 +6,6 @@ from vllm_ascend.worker.v2.attn_utils import (
     get_kv_cache_spec,
 )
 
-
-def _get_kv_cache_spec_for_runner(self):
-    return get_kv_cache_spec(self.vllm_config)
-
-
 vllm.v1.worker.gpu.attn_utils._allocate_kv_cache = _allocate_kv_cache
 vllm.v1.worker.gpu.attn_utils._reshape_kv_cache = _reshape_kv_cache_v2
 vllm.v1.worker.gpu.model_runner.get_kv_cache_spec = get_kv_cache_spec
-vllm.v1.worker.gpu.model_runner.GPUModelRunner.get_kv_cache_spec = (
-    _get_kv_cache_spec_for_runner
-)
-vllm.v1.worker.gpu_model_runner.GPUModelRunner.get_kv_cache_spec = (
-    _get_kv_cache_spec_for_runner
-)
