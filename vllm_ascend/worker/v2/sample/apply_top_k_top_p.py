ea32c185d38c4ad2f2094e56eeefa4b83a899e43
AuroraEmiya <Sakura.iostream@gmail.com>
2026-08-25 11:20:45 +0800

    [Refactor][Delete][MRV2]Patch mrv2 apply_top_k_top_p with cann operator
    Signed-off-by: AuroraEmiya <Sakura.iostream@gmail.com>

---
+++ vllm_ascend/worker/v2/sample/apply_top_k_top_p.py
@@ -0,0 +1,12 @@
+import torch
+import torch_npu  # noqa: F401
+
+
+def apply_top_k_top_p(
+    logits: torch.Tensor, k: torch.Tensor | None, p: torch.Tensor | None
+) -> torch.Tensor:
+    
+    if k is None and p is None:
+        return logits
+    #use cann ops
+    return torch_npu.npu_top_k_top_p(logits, k, p)
